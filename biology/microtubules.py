6def5a27392834136d3fcb5ceed513cd22b7c930
Sharon Minsuk <sharon@sharonminsuk.com>
2022-12-08 01:48:50 -0800

    Another external force improvement: account for radius
    (Also, not NamedTuple after all, because I needed
    mutability. "dataclass" to the rescue.)

--- biology/microtubules.py
+++ biology/microtubules.py
@@ -1,6 +1,6 @@
 """Simulate the towing of the margin toward the vegetal pole by microtubule arrays in the yolk cells"""
+from dataclasses import dataclass
 import math
-from typing import NamedTuple
 
 import tissue_forge as tf
 from epiboly_init import Big, LeadingEdge
@@ -10,6 +10,8 @@ from utils import tf_utils as tfu
 def update_tangent_forces(magnitude: int) -> None:
     """Note that once this has run, turning it off does not remove existing forces. Use remove_tangent_forces().
     
+    Deprecated.
+    
     Still to do! This needs a stopping criterion. Based on angle of phi? Or distance of particle
     from the vegetal pole? Needs criterion both for the individual particle, and for when all of them
     have arrived.
@@ -18,7 +20,7 @@ def update_tangent_forces(magnitude: int) -> None:
     big_particle = Big.particle(0)
     for p in LeadingEdge.items():
         r, theta, phi = p.sphericalPosition(particle=big_particle)
-        tangent_phi = phi + math.pi / 2
+        tangent_phi = phi + cfg.pi_over_2
         tangent_force_vec: tf.fVector3 = tfu.cartesian_from_spherical([magnitude, theta, tangent_phi])
 
         # The assignment runs into the copy-constructor bug! So change to plain list
@@ -31,10 +33,16 @@ def remove_tangent_forces() -> None:
     print("Tangent forces removed")
 
 def apply_even_tangent_forces(magnitude: int) -> None:
-    class ParticleData(NamedTuple):
+    """To apply force evenly, must take into account not only the variation in density around the marginal ring, but
+    also the effect of radius: if density is measured by delta theta between neighboring edge cells, then a region of
+    cells will seem to get less "dense" as it approaches the vegetal pole because the theta between them will increase.
+    """
+    @dataclass
+    class ParticleData:
         phandle: tf.ParticleHandle
         theta: float
         phi: float
+        weight: float = 0
         
     def get_particle_data(p: tf.ParticleHandle) -> ParticleData:
         r, theta, phi = p.sphericalPosition(particle=big_particle)
@@ -46,18 +54,29 @@ def apply_even_tangent_forces(magnitude: int) -> None:
     particle_data_list: list[ParticleData] = [get_particle_data(p) for p in LeadingEdge.items()]
     sorted_on_theta: list[ParticleData] = sorted(particle_data_list, key=lambda data: data.theta)
 
+    # First loop: collect (and sum) all the weights
     previous_particle_data: ParticleData = sorted_on_theta[-1]
     before_previous_particle_data: ParticleData = sorted_on_theta[-2]
     previous_theta: float = previous_particle_data.theta - cfg.two_pi
     before_previous_theta: float = before_previous_particle_data.theta - cfg.two_pi
     particle_data: ParticleData
+    weight_total: float = 0
     for particle_data in sorted_on_theta:
-        arc: float = (particle_data.theta - before_previous_theta) / 2
-        mag: float = total_force * arc / cfg.two_pi
-        tangent_phi = previous_particle_data.phi + math.pi / 2
-        tangent_force_vec: tf.fVector3 = tfu.cartesian_from_spherical([mag, previous_theta, tangent_phi])
-        previous_particle_data.phandle.force_init = tangent_force_vec.as_list()
+        arc: float = (particle_data.theta - before_previous_theta)
+        # The relevant "radius" of the leading edge "circle" is actually the distance (or arc) from the vegetal pole:
+        radius: float = math.pi - previous_particle_data.phi
+        weight: float = radius * arc
+        previous_particle_data.weight = weight
+        weight_total += weight
         
         before_previous_theta = previous_theta
         previous_particle_data = particle_data
         previous_theta = particle_data.theta
+        
+    # Second loop: now that we have all the weights and the total, we can calculate the forces
+    for particle_data in sorted_on_theta:
+        mag: float = total_force * particle_data.weight / weight_total
+        tangent_phi = particle_data.phi + cfg.pi_over_2
+        tangent_force_vec: tf.fVector3 = tfu.cartesian_from_spherical([mag, particle_data.theta, tangent_phi])
+        particle_data.phandle.force_init = tangent_force_vec.as_list()
+
