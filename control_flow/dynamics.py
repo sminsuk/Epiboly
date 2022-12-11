79d0aede00b3212020911df6b2d3b9a65f317faf
Sharon Minsuk <sharon@sharonminsuk.com>
2022-12-10 21:29:36 -0800

    More minor cruft removal

--- control_flow/dynamics.py
+++ control_flow/dynamics.py
@@ -2,11 +2,6 @@
 
 Creates events that are run every simulation step. By default, they do nothing,
 until execute_repeatedly() is called, creating a task list.
-
-The on_time event is for actions that involve iterating over multiple particles at once.
-The on_particle events are for actions that involve a single particle.
-on_particle events only take a *single* ParticleType. So if you want to run an event on
-more than one type, you have to turn on that event for each one.
 """
 from collections.abc import Callable
 from typing import TypedDict
@@ -48,7 +43,6 @@ def execute_repeatedly(tasks: list[Task] = None) -> None:
         args: dict = task["args"]
         print(f"setting up invoke: {invoke.__name__} with args: {args}")
 
-######## Private ########
 _tasks: list[Task] = []
 
 def _master_event(evt: tf.event.TimeEvent) -> int:
@@ -70,27 +64,5 @@ def _master_event(evt: tf.event.TimeEvent) -> int:
     
     return 0
 
-# def _master_particle_event(evt):
-#     """This runs every simulation step. Ignore evt and let it run forever, but pass the particle to the invoked actions.
-#
-#     Use for actions that act on a single particle.
-#     evt contains the particle information.
-#     For now, hard-coding that this acts on the two types, Little and LeadingEdge. I'll probably generalize
-#     this, and set up a way to launch events for any generic set of ParticleTypes.
-#     Also for now, using same master event for all types. If their behaviors get very divergent, might end up
-#     being cleaner to have one master_particle_event for each type. Alternatively, different actions for each type.
-#     """
-#     global _bond_maintenance
-#     if _bond_maintenance[evt.targetType.name]:
-#         try:
-#             _maintain_bonds(evt.targetParticle)
-#         except Exception as e:
-#             su.exception_handler(e, _master_particle_event.__name__)
-
-
 # This will happen as soon as it's imported (after the tf.init)
 tf.event.on_time(period=tf.Universe.dt, invoke_method=_master_event)
-
-# These will happen as soon as they're imported (after the tf.init)
-# tf.event.on_particletime(period=10 * tf.Universe.dt, invoke_method=_master_particle_event, ptype=Little)
-# tf.event.on_particletime(period=10 * tf.Universe.dt, invoke_method=_master_particle_event, ptype=LeadingEdge)
