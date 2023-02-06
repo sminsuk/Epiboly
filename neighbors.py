8cb889f532f8294cad94954946815a56d9ae618b
Sharon Minsuk <sharon@sharonminsuk.com>
2023-02-05 22:55:05 -0800

    Cruft removal: old neighbor-finding algorithm

--- neighbors.py
+++ neighbors.py
@@ -9,82 +9,11 @@ from utils import tf_utils as tfu
 import config as cfg
 from epiboly_init import Little, LeadingEdge, Big
 
-def _unshadowed_neighbors(p: tf.ParticleHandle, distance_factor: float) -> list[tf.ParticleHandle]:
-    """Not nearest neighbors, but best neighbors.
+def find_neighbors(p: tf.ParticleHandle, distance_factor: float, sort: bool = False) -> list[tf.ParticleHandle]:
+    """Find neighbors of particle p
     
-    p: particle_handle
-    distance_factor: when multiplied by the particle radius, gives the distance to search.
-    returns: neighbors of p in a plain python list, ordered by increasing distance from p
-    
-    Bridges across gaps.
-    Avoids having no neighbors in a particular direction.
-    Avoids having multiple tiers of neighbors all in the same direction.
-    """
-    
-    def is_in_shadow(candidate, neighbor, particle):
-        """Is the candidate "hidden" from particle by the shadow cast by neighbor?"""
-        
-        # Where p, n, c stand for particle, neighbor, candidate, respectively:
-        pn_vector = neighbor.position - p.position
-        pn_distance = p.distance(neighbor)
-        pc_distance = p.distance(candidate)
-        cn_distance_ratio = pc_distance / pn_distance
-        
-        # and s stands for shadow:
-        # center of shadow cone, at same distance as the candidate:
-        ps_vector = pn_vector * cn_distance_ratio
-        s_position = p.position + ps_vector
-        s_radius = Little.radius * cn_distance_ratio
-        
-        # how far is the candidate from the shadow center?
-        cx, cy, cz = candidate.position
-        sx, sy, sz = s_position
-        cs_distance = math.sqrt((sx - cx) ** 2 + (sy - cy) ** 2 + (sz - cz) ** 2)
-        return cs_distance < s_radius
-        
-        # Dropping down distance_factor to 1.5 got rid
-    
-    # of all the crossings (2.0 reduced the number, but did not eliminate).
-    # And at this distance, I *still* got occasional false positives, which maybe are worse than
-    # false negatives? So I guess I will stick with the simpler method for now, the native one.
-    search_distance = Little.radius * distance_factor
-    
-    # Capture all potential neighbors within the region; ordered by distance from p (nearest neighbor last)
-    candidates = p.neighbors(search_distance, [].extend([Little, LeadingEdge]))
-    candidates = sorted(candidates, key=lambda candidate: candidate.distance(p), reverse=True)
-    
-    # testing
-    #     print("particle.position = ", p.position.as_list())
-    #     for candidate in candidates:
-    #         print("distance =", candidate.distance(p))
-    
-    neighbors = []
-    while candidates:
-        # accept the nearest of the candidates as a neighbor
-        neighbors.append(candidates.pop())
-        
-        # discard any remaining candidates that are in the shadow of an accepted neighbor
-        surviving_candidates = []
-        for candidate in candidates:
-            shadowed = False
-            for neighbor in neighbors:
-                if is_in_shadow(candidate, neighbor, p):
-                    shadowed = True
-                    break
-            if not shadowed:
-                surviving_candidates.append(candidate)
-        candidates = surviving_candidates
-    
-    # We now have the neighbors of particle p. These are ordered by increasing distance from p.
-    return neighbors
-
-def _native_neighbors(p: tf.ParticleHandle, distance_factor: float, sort: bool = False) -> list[tf.ParticleHandle]:
-    """Native method of Tissue Forge for finding neighbors of particle p
-    
-    p: particle_handle
-    distance_factor: when multiplied by the particle radius, gives the distance to search.
+    distance_factor: search out to this multiple of particle radius
     sort: if True, return results ordered by increasing distance from p.
-    returns: neighbors of p in a plain python list, ordered by increasing distance from p
     """
     # Get all the particles within the threshold distance of p.
     neighbors = p.neighbors(distance_factor * p.radius, [].extend([Little, LeadingEdge]))
@@ -92,35 +21,14 @@ def _native_neighbors(p: tf.ParticleHandle, distance_factor: float, sort: bool =
         neighbors = sorted(neighbors, key=lambda neighbor: neighbor.distance(p))
     return neighbors
 
-# noinspection PyUnreachableCode
-def find_neighbors(p: tf.ParticleHandle, distance_factor: float = 1.5, sort: bool = False) -> list[tf.ParticleHandle]:
-    """A central place to keep the decision of which neighbor algorithm to use, consistently throughout the program.
-    
-    p: particle_handle
-    distance_factor: when multiplied by the particle radius, gives the distance to search.
-        Default value works as basic threshold for making bonds while minimizing crossings.
-    sort: if True, return results ordered by increasing distance from p. Applies only to _native_neighbors(),
-        since sorting is non-optional in _unshadowed_neighbors().
-    returns: neighbors of p in a plain python list. If sort == True, list is ordered by increasing distance from p
-    """
-    neighbors: list[tf.ParticleHandle]
-    
-    # Pick one of these methods (both return neighbors ordered by increasing distance from particle):
-    if False:
-        neighbors = _unshadowed_neighbors(p, distance_factor)
-    else:
-        neighbors = _native_neighbors(p, distance_factor, sort)
-    return neighbors
-
 def get_non_bonded_neighbors(phandle: tf.ParticleHandle,
                              distance_factor: float, sort: bool = False) -> list[tf.ParticleHandle]:
-    """Not quite the inverse of particleHandle.getBondedNeighbors()
+    """Return list of neighbors, but excluding any that the particle is already bonded to
     
-    phandle: particleHandle
-    distance_factor: search out to this multiple of radius
-    sort: if True, return results ordered by increasing distance from p.
-    returns: list of neighbors, using one of the neighbor algorithms in this module, but excluding any that
-        the particle is already bonded to.
+    (Sort of the inverse of particleHandle.getBondedNeighbors().)
+    
+    distance_factor: search out to this multiple of particle radius
+    sort: return results ordered by increasing distance from particle.
     """
     my_bonded_neighbor_ids: list[int]
     neighbors: list[tf.ParticleHandle]
@@ -340,7 +248,7 @@ def paint_neighbors():
         p.style.color = found_color
         
         # Find the neighbors of this particle
-        neighbors = find_neighbors(p)
+        neighbors = find_neighbors(p, distance_factor=cfg.min_neighbor_initial_distance_factor)
         
         for neighbor in neighbors:
             if neighbor not in found_particles:
