bc572655a610863852e7adbe0dafc499dff6c280
Sharon Minsuk <sharon@sharonminsuk.com>
2023-02-05 21:26:25 -0800

    Cruft removal: comment

--- config.py
+++ config.py
@@ -24,18 +24,8 @@ screenshot_export_interval: int = 10
 # Angle bonds cause instability and big waviness in the leading edge
 angle_bonds_enabled: bool = True
 
-# Just while determining empirically, the right number of interior particles to use.
-# If there are too many, with frozen LeadingEdge, they'll pop past. If too few,
-# they'll never fill the space no matter how long they equilibrate. Once the right
-# number is determined, they should fill the space without popping through. Then
-# Can comment this out and never use it again. (Or, perhaps will always use it during
-# equilibration step, and release it afterward.)
-# With this approach, instead of guessing how many particles to use and letting the
-# leading edge get pushed downward by their expansion to an appropriate place, we'll
-# set the location we *want* the leading edge, and get the right number of particles
-# in there.
-# LeadingEdge.frozen_z = True
-epiboly_initial_percentage: int = 43     # real value for the misnomer "30% epiboly")
+# real value for the misnomer "30% epiboly")
+epiboly_initial_percentage: int = 43
 
 # How many leading edge and interior cells to make
 num_leading_edge_points: int = 110
